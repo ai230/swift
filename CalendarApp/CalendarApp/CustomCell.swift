af7998c0b4e4aac991206ecfe2de66a9d2328fc9
AiYamamoto <ay36230@gmail.com>
2017-06-29 10:11:54 -0700

    created a folder

---
+++ CalendarApp/CalendarApp/CustomCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCell.swift
+//  CalendarApp
+//
+//  Created by AiYamamoto on 2017-06-24.
+//  Copyright © 2017 Ai Yamamoto. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var selectedView: UIView!
+
+}
